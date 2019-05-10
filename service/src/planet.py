501422a63975ac4527b0b4cc4268115443074ab2
sbritocorral <s.britocorral@gmail.com>
2019-05-10 12:29:48 +0200

    Add location to returning dict. Reformat.

--- service/src/planet.py
+++ service/src/planet.py
@@ -1,32 +1,28 @@
-import string
-import random
 from app import db
 
 
 class Planet(db.Model):
-    id = db.Column('dateaseId', db.Integer, primary_key = True)
+    id = db.Column('dateaseId', db.Integer, primary_key=True)
     planetId = db.Column(db.String(100))
     name = db.Column(db.String(100))
-    declination = db.Column(db.String(100))  
+    declination = db.Column(db.String(100))
     rightAscension = db.Column(db.String(100))
     location = db.Column(db.String(100))
     flag = db.Column(db.String(100))
 
-
-    def __init__(self, name, declination, rightAscension, flag):
+    def __init__(self, name, declination, right_ascension, flag):
         self.name = name
         # self.planetId = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(32))
         self.declination = declination
-        self.rightAscension = rightAscension
+        self.rightAscension = right_ascension
         self.flag = flag
 
     def to_dict(self):
-        return{
-            "planetId" : self.planetId,
-            "name" : self.name,
-            "declination" : self.declination,
-            "rightAscension" : self.rightAscension,
-            "flag" : self.flag
+        return {
+            "planetId": self.planetId,
+            "name": self.name,
+            "declination": self.declination,
+            "rightAscension": self.rightAscension,
+            "location": self.location,
+            "flag": self.flag
         }
-
-    
