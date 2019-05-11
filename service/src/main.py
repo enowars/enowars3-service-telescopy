b24fbc853ccc90ddb8461e7213d7e08900809839
sbritocorral <s.britocorral@gmail.com>
2019-05-11 19:38:55 +0200

    Update ticket checker

--- service/src/main.py
+++ service/src/main.py
@@ -6,4 +6,4 @@ app.register_blueprint(routes)
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host='127.0.0.1', port=8000, debug=True)
--- service/src/ticketCheck.c
+++ service/src/ticketCheck.c
@@ -1,41 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
-int main(int argc,char* argv[]) 
+int main(int argc, char *argv[])
 {
-   printf("start checking!");
-	int n = 0;
-	if(argc == 2){
-		n = atoi(argv[1]);
-      n = -n;
-		
-	}else{
-		return 0;
-      printf("nope");
-	}
-	printf("%d",INT_MAX );
-	if (n > INT_MAX || n < 99999999){
-            printf("nope out of range");
-
-		return 0;
-	}
-   int c = 2;
-    
-   for ( c = 2 ; c <= n - 1 ; c++ )
-   {
-      if ( n%c == 0 )
+      printf("Checking the ticket ...\n");
+      long long n = 0;
+      if (argc == 2)
       {
-
-         printf("nope! can be devided");
-     break;
+            n = atol(argv[1]);
+            n = -n;
+            printf("n: %lld \n", n);
       }
-   }
-   if ( c == n ){
-            printf("yup");
+      else
+      {
+            printf("wrong arguments :( \n");
+            return 0;  
+      }
+      if (n < 10000000000)
+      {
+            printf("nope out of range\n");
+            return 0;
+      }
+      long long c = 2;
 
-      return 1;
-   }
-                printf("nope!");
+      for (c = 2; c <= n - 1; c++)
+      {
+            if (n % c == 0)
+            {
+                  break;
+            }
+      }
+      if (c == n)
+      {
+            printf("yup valid ticket\n");
+            return 1;
+      }
+      printf("nope wrong ticket!\n");
 
-   return 0;
+      return 0;
 }
